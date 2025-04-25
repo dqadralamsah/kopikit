ea3299bf2533cde9bd368574c5a0887cfa481518
dqadralamsah <dqadralamsah@gmail.com>
2025-04-25 18:35:30 +0700

    feat(menu-section): new menu section

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Mobile Navbar
 const navMobileWrapper = document.querySelector(
   ".nav-mobile-wrapper"
 );
@@ -5,3 +6,28 @@ const navMobileWrapper = document.querySelector(
 document.querySelector(".nav-bars").onclick = () => {
   navMobileWrapper.classList.toggle("active");
 };
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // Breakpoints
+  breakpoints: {},
+});
